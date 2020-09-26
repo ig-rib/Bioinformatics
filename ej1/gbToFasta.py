f00cb7898e8fec6cb952ebfd1d84c2c6ef71a4f2
Ignacio Ribas <iribas@itba.edu.ar>
2020-09-26 14:22:59 -0300

    Ej 1 done

--- ej1/gbToFasta.py
+++ ej1/gbToFasta.py
@@ -1,10 +1,20 @@
 #!/bin/python3
 
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Seq import translate
+from Bio.SeqRecord import SeqRecord
+from utils import translatemRNASequence
 
-with open("sirt6.gb", "rU") as input_handle:
-    with open("sirt6.fasta", "w") as output_handle:
+with open("ej1/sirt6.gb", "rU") as input_handle:
+    with open("ej1/sirt6.fasta", "w") as output_handle:
         sequences = SeqIO.parse(input_handle, "genbank")
-        count = SeqIO.write(sequences, output_handle, "fasta")
+        # for sequence in sequences:
+        #     print(sequence.translate())
+        # translatedSequences = list(map(lambda x: x.translate(), sequences))
+        # count = SeqIO.write(translatedSequences, output_handle, "fasta")
+        for seq in sequences:
+            translatemRNASequence(seq, "ej1/sirt6.fasta")
 
-print("Converted %i records" % count)
+# print("Converted %i records" % count)
+
---
+++ ej1/utils.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Seq import translate
+from Bio.SeqRecord import SeqRecord
+
+def translatemRNASequence(seq, filename="output", translationTable=1):
+
+    allPossibilities = []
+
+    for frame in range(3):
+        trans = seq[frame:].translate(to_stop=True)
+        allPossibilities.append(trans)
+
+    i = 0
+    for protein in allPossibilities:
+        i = i + 1
+        # currentProtein = Seq(protein)
+
+        # currentProteinRecord = SeqRecord(protein, name=seq.name)
+        protein.id = seq.id + "." + str(i) + " -> Generated Protein;"
+        protein.description = seq.description + "; frame " + str(i)
+        # with open(filename, "w") as output_handle:
+        #     SeqIO.write(protein, output_handle, "fasta")
+    with open(filename, "w") as output_handle:
+        SeqIO.write(allPossibilities, output_handle, "fasta")      
