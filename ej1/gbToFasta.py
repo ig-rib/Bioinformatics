313a6ca7e7c9ace1bf225abf64d995986acd16b0
Ignacio Ribas <iribas@itba.edu.ar>
2020-09-26 14:48:16 -0300

    Multiseq support for ej1

--- ej1/gbToFasta.py
+++ ej1/gbToFasta.py
@@ -5,16 +5,27 @@ from Bio.Seq import Seq
 from Bio.Seq import translate
 from Bio.SeqRecord import SeqRecord
 from utils import translatemRNASequence
+import sys
 
-with open("ej1/sirt6.gb", "rU") as input_handle:
-    with open("ej1/sirt6.fasta", "w") as output_handle:
+if len(sys.argv) != 2:
+    print("Usage: gbToFasta [genBankFilePath]")
+else:
+    genBankFilePath = sys.argv[1]
+    outputFilePathSplit = genBankFilePath.split('.')
+    if outputFilePathSplit[-1] in ("gb", "gbk"):
+        outputFilePathSplit.pop()
+    outputFilePath = ".".join(outputFilePathSplit)
+
+    with open(genBankFilePath, "rU") as input_handle:
         sequences = SeqIO.parse(input_handle, "genbank")
         # for sequence in sequences:
         #     print(sequence.translate())
         # translatedSequences = list(map(lambda x: x.translate(), sequences))
         # count = SeqIO.write(translatedSequences, output_handle, "fasta")
+        i = 0
         for seq in sequences:
-            translatemRNASequence(seq, "ej1/sirt6.fasta")
+            i += 1
+            translatemRNASequence(seq, outputFilePath + f"-seq{i}.fasta")
 
-# print("Converted %i records" % count)
+    # print("Converted %i records" % count)
 
