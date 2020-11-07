d2ff0be4e2de6ab50e091bc3d9a9c532e47664f0
Ignacio Ribas <iribas@itba.edu.ar>
2020-11-07 00:08:01 -0300

    Done with ex5

--- ej5/Ex5.py
+++ ej5/Ex5.py
@@ -1,13 +1,14 @@
 import os, sys
 
-outputFilename = "orfs.fasta"
+outputFilename = "ej5-1-orfs.fasta"
 
 if len(sys.argv) < 2:
     print("Usage: python3 Ex5.py filename [outputFilename]")
     exit(1)
 filename = sys.argv[1]
-
 if len(sys.argv) > 2:
     outputFilename = sys.argv[2]
 
 os.system("getorf " + filename + " -outseq " + outputFilename)
+os.system("sudo prosextract ./")
+os.system("patmatmotifs " + filename + " -full 1  -outfile ej5-2.patmatmotifs")
