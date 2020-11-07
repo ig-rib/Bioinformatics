e47b61fe0f40098e26a60f12f05ad337213d7daf
Ignacio Ribas <iribas@itba.edu.ar>
2020-11-06 22:39:48 -0300

    ej5 fix

--- ej5/Ex5.py
+++ ej5/Ex5.py
@@ -1,3 +1,13 @@
-import os
+import os, sys
 
-os.system("getorf ej5/sirt6rna2.fasta -outseq ej5/orfs.fasta")
+outputFilename = "orfs.fasta"
+
+if len(sys.argv) < 2:
+    print("Usage: python3 Ex5.py filename [outputFilename]")
+    exit(1)
+filename = sys.argv[1]
+
+if len(sys.argv) > 2:
+    outputFilename = sys.argv[2]
+
+os.system("getorf " + filename + " -outseq " + outputFilename)
