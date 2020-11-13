150b72909966d1c646395587878fd4cd2ab6502f
Ignacio Ribas <iribas@itba.edu.ar>
2020-11-13 20:42:59 -0300

    moved Ex5 results to ex5 folder

--- ej5/Ex5.py
+++ ej5/Ex5.py
@@ -11,4 +11,4 @@ if len(sys.argv) > 2:
 
 os.system("getorf " + filename + " -outseq " + outputFilename)
 os.system("sudo prosextract ./")
-os.system("patmatmotifs " + filename + " -full 1  -outfile ej5-2.patmatmotifs")
+os.system("patmatmotifs " + filename + " -full 1  -outfile ./ej5/ej5-2.patmatmotifs")
